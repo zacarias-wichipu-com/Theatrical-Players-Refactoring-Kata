0fab2e5b90d5ac576e78ffdd2c58daa45030ceb8
Zacarías <zacarias@wichipu.com>
2024-02-05 12:09:31 +0100

    refactor: Improved amount naming terms.

--- src/StatementPrinter.php
+++ src/StatementPrinter.php
@@ -14,7 +14,7 @@ class StatementPrinter
      */
     public function print(Invoice $invoice, array $plays): string
     {
-        $totalAmount = new Amount(amount: 0);
+        $invoiceAmount = new Amount(amount: 0);
         $credit = new Credit(credit: 0);
 
         $result = "Statement for {$invoice->customer}\n";
@@ -27,32 +27,24 @@ class StatementPrinter
                 case 'tragedy':
                     $performanceAmount = new Amount(amount: 40000);
                     if ($performance->audience > 30) {
-                        $performanceAmount = $performanceAmount->add(
-                            amountToAdd: new Amount(
-                                amount: 1000 * ($performance->audience - 30)
-                            )
-                        );
+                        $extraAudiencePerformanceAmount = new Amount(amount: 1000 * ($performance->audience - 30));
+                        $performanceAmount = $performanceAmount->add(amountToAdd: $extraAudiencePerformanceAmount);
                     }
                     break;
                 case 'comedy':
                     $performanceAmount = new Amount(amount: 30000);
                     if ($performance->audience > 20) {
-                        $performanceAmount = $performanceAmount->add(
-                            amountToAdd: new Amount(
-                                amount: 10000 + 500 * ($performance->audience - 20)
-                            )
+                        $extraAudiencePerformanceAmount = new Amount(
+                            amount: 10000 + 500 * ($performance->audience - 20)
                         );
+                        $performanceAmount = $performanceAmount->add(amountToAdd: $extraAudiencePerformanceAmount);
                     }
-                    $performanceAmount = $performanceAmount->add(
-                        amountToAdd: new Amount(
-                            amount: 300 * $performance->audience
-                        )
-                    );
+                    $audiencePerformanceAmount = new Amount(amount: 300 * $performance->audience);
+                    $performanceAmount = $performanceAmount->add(amountToAdd: $audiencePerformanceAmount);
                     break;
                 default:
                     throw new Error("Unknown type: {$play->type}");
             }
-
             // add volume credit
             $creditByAudience = new Credit(credit: max($performance->audience - 30, 0));
             $credit = $credit->add($creditByAudience);
@@ -61,15 +53,12 @@ class StatementPrinter
                 $creditByType = new Credit(credit: (int)floor($performance->audience / 5));
                 $credit = $credit->add($creditByType);
             }
-
             // print line for this order
             $result .= "  {$play->name}: {$format->formatCurrency($performanceAmount->value() / 100, 'USD')} ";
             $result .= "({$performance->audience} seats)\n";
-
-            $totalAmount = $totalAmount->add(amountToAdd: $performanceAmount);
+            $invoiceAmount = $invoiceAmount->add(amountToAdd: $performanceAmount);
         }
-
-        $result .= "Amount owed is {$format ->formatCurrency($totalAmount->value() / 100, 'USD')}\n";
+        $result .= "Amount owed is {$format ->formatCurrency($invoiceAmount->value() / 100, 'USD')}\n";
         $result .= "You earned {$credit} credits";
 
         return $result;
