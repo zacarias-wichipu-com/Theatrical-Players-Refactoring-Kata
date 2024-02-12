03589d95966bba9e5e076d0f903ad6761c74cb68
Zacarías <zacarias@wichipu.com>
2024-02-12 18:13:10 +0100

    refactor: Extracted method to get amount format currency.
    Object Calisthenics: One Dot Per Line.

--- src/StatementPrinter.php
+++ src/StatementPrinter.php
@@ -8,6 +8,7 @@ use NumberFormatter;
 
 readonly class StatementPrinter
 {
+    public const string CURRENCY_USD = 'USD';
     private NumberFormatter $numberFormatter;
 
     public function __construct()
@@ -20,10 +21,11 @@ readonly class StatementPrinter
         $invoiceAmount = new Amount(amount: 0);
         $invoiceCredit = new Credit(credit: 0);
         $invoiceOutput = "Statement for {$invoice->customer}\n";
+        /** @var Performance $performance */
         foreach ($invoice->performances as $performance) {
             $play = $plays->getById($performance->playId);
             $performanceOutput = "  {$play->name}: ";
-            $performanceOutput .= "{$this->numberFormatter->formatCurrency($performance->amount($play)->value() / 100, 'USD')} ";
+            $performanceOutput .= "{$this->formatCurrencyAmount($performance->amount($play))} ";
             $performanceOutput .= "({$performance->audience} seats)\n";
             $invoiceAmount = $invoiceAmount->add(
                 amountToAdd: $performance->amount(play: $play)
@@ -31,9 +33,13 @@ readonly class StatementPrinter
             $invoiceCredit = $invoiceCredit->add(creditToAdd: $performance->credit($play));
             $invoiceOutput .= $performanceOutput;
         }
-        $invoiceOutput .= "Amount owed is {$this->numberFormatter ->formatCurrency($invoiceAmount->value() / 100, 'USD')}\n";
+        $invoiceOutput .= "Amount owed is {$this->formatCurrencyAmount($invoiceAmount)}\n";
         $invoiceOutput .= "You earned {$invoiceCredit} credits";
         return $invoiceOutput;
     }
 
+    private function formatCurrencyAmount(Amount $amount): string|false
+    {
+        return $this->numberFormatter->formatCurrency($amount->value() / 100, self::CURRENCY_USD);
+    }
 }
