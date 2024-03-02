cb61e4b6f7ec495c05c4fddef4200eab0250f709
Zacarías <zacarias@wichipu.com>
2024-03-02 07:52:17 +0100

    refactor: Extracted to methos the filling invoice responsibility.

--- src/StatementPrinter.php
+++ src/StatementPrinter.php
@@ -8,19 +8,25 @@ readonly class StatementPrinter
 {
     public function print(Invoice $invoice, Plays $plays): string
     {
+        $statement = new Statement();
+        $this->fillInvoice($statement, $invoice, $plays);
+        return $statement->print();
+    }
+
+    private function fillInvoice(Statement $statement, Invoice $invoice, Plays $plays): void
+    {
         $invoiceAmount = new Amount(amount: 0);
         $invoiceCredit = new Credit(credit: 0);
-        $statement = new Statement();
         $statement->fillCustomer(customer: $invoice->customer);
         /** @var Performance $performance */
         foreach ($invoice->performances as $performance) {
             $play = $plays->getById($performance->playId);
             $invoiceAmount = $invoiceAmount->add(amountToAdd: $performance->amount(play: $play));
             $invoiceCredit = $invoiceCredit->add(creditToAdd: $performance->credit($play));
-            $statement->fillLine(name: $play->name, amount: $performance->amount($play), audience: $performance->audience);
+            $statement->fillLine(name: $play->name, amount: $performance->amount($play),
+                audience: $performance->audience);
         }
         $statement->fillAmount(amount: $invoiceAmount);
         $statement->fillCredit(credit: $invoiceCredit);
-        return $statement->print();
     }
 }
