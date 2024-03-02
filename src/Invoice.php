8a690dbe27914dcf35d4887b9a322e3423467dcf
Zacarías <zacarias@wichipu.com>
2024-03-02 07:53:25 +0100

    refactor: Moved the filling invoice method from StatementPrinter object to Invoice object.

--- src/Invoice.php
+++ src/Invoice.php
@@ -11,4 +11,21 @@ class Invoice
         public Performances $performances
     ) {
     }
+
+    public function fillInvoice(Statement $statement, Plays $plays): void
+    {
+        $invoiceAmount = new Amount(amount: 0);
+        $invoiceCredit = new Credit(credit: 0);
+        $statement->fillCustomer(customer: $this->customer);
+        /** @var Performance $performance */
+        foreach ($this->performances as $performance) {
+            $play = $plays->getById($performance->playId);
+            $invoiceAmount = $invoiceAmount->add(amountToAdd: $performance->amount(play: $play));
+            $invoiceCredit = $invoiceCredit->add(creditToAdd: $performance->credit($play));
+            $statement->fillLine(name: $play->name, amount: $performance->amount($play),
+                audience: $performance->audience);
+        }
+        $statement->fillAmount(amount: $invoiceAmount);
+        $statement->fillCredit(credit: $invoiceCredit);
+    }
 }
