5a3f9dbea5885e0908f1d8a6c50d69cda7b07bcd
Zacarías <zacarias@wichipu.com>
2024-03-02 07:43:18 +0100

    feat: Remove old StatementPrinter lines with responsibilities for filling out the invoice.

--- src/StatementPrinter.php
+++ src/StatementPrinter.php
@@ -11,25 +11,17 @@ readonly class StatementPrinter
         $invoiceAmount = new Amount(amount: 0);
         $invoiceCredit = new Credit(credit: 0);
         $statement = new Statement();
-        $invoiceOutput = "Statement for {$invoice->customer}\n";
         $statement->fillCustomer($invoice->customer);
         /** @var Performance $performance */
         foreach ($invoice->performances as $performance) {
             $play = $plays->getById($performance->playId);
-            $performanceAmount = $performance->amount($play);
-            $performanceOutput = "  {$play->name}: ";
-            $performanceOutput .= "{$performanceAmount->USDFormatCurrency()} ";
-            $performanceOutput .= "({$performance->audience} seats)\n";
             $statement->fillLine($play->name, $performance->amount($play), $performance->audience);
             $invoiceAmount = $invoiceAmount->add(
                 amountToAdd: $performance->amount(play: $play)
             );
             $invoiceCredit = $invoiceCredit->add(creditToAdd: $performance->credit($play));
-            $invoiceOutput .= $performanceOutput;
         }
-        $invoiceOutput .= "Amount owed is {$invoiceAmount->USDFormatCurrency()}\n";
         $statement->fillAmount($invoiceAmount);
-        $invoiceOutput .= "You earned {$invoiceCredit} credits";
         $statement->fillCredit($invoiceCredit);
         return $statement->print();
     }
