6a780ad18d810e674744d1dfa36a6b7afe457ec1
Zacarías <zacarias@wichipu.com>
2024-03-02 08:14:07 +0100

    refactor: Extracted invoice amount computation to method.

--- src/Invoice.php
+++ src/Invoice.php
@@ -14,17 +14,24 @@ final readonly class Invoice
 
     public function fill(Statement $statement, Plays $plays): void
     {
-        $invoiceAmount = new Amount(amount: 0);
         $invoiceCredit = new Credit(credit: 0);
         $statement->fillCustomer(customer: $this->customer);
         foreach ($this->performances as $performance) {
             $play = $plays->getById($performance->playId);
-            $invoiceAmount = $invoiceAmount->add(amountToAdd: $performance->amount(play: $play));
             $invoiceCredit = $invoiceCredit->add(creditToAdd: $performance->credit($play));
             $statement->fillLine(name: $play->name, amount: $performance->amount($play),
                 audience: $performance->audience);
         }
-        $statement->fillAmount(amount: $invoiceAmount);
+        $statement->fillAmount(amount: $this->amount($plays));
         $statement->fillCredit(credit: $invoiceCredit);
+    }
+
+    private function amount(Plays $plays): Amount {
+        $invoiceAmount = new Amount(amount: 0);
+        foreach ($this->performances as $performance) {
+            $play = $plays->getById($performance->playId);
+            $invoiceAmount = $invoiceAmount->add(amountToAdd: $performance->amount(play: $play));
+        }
+        return $invoiceAmount;
     }
 }
