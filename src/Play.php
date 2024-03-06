35704e9029fe37aebf4592ac0c5a8de215fadbae
Zacarías <zacarias@wichipu.com>
2024-03-06 07:28:31 +0100

    refactor: Remove specialised methods from Play class.

--- src/Play.php
+++ src/Play.php
@@ -27,47 +27,8 @@ readonly class Play implements Stringable
     {
         return $this->title;
     }
-
-    public function credit(int $audience): Credit
-    {
-        if ($this->genre === 'comedy') {
-            return new Credit(credit: (int) floor($audience / 5));
-        }
-        return new Credit(credit: 0);
-    }
-
-    public function amount(int $audience): Amount
-    {
-        if ($this->genre === 'tragedy') {
-            return $this->tragedyAmount($audience);
-        }
-        throw new Error("Unknown genre: {$this->genre}");
-    }
-
     public function __toString(): string
     {
         return (string) $this->title.' : '.$this->genre;
     }
-
-    protected function tragedyAmount(int $audience): Amount
-    {
-        $feeAmount = $this->tragedyFeeAmount();
-        return $feeAmount->add(
-            amountToAdd: $this->tragedyExtraAmountByAudience($audience)
-        );
-    }
-
-    protected function tragedyFeeAmount(): Amount
-    {
-        return new Amount(amount: 40000);
-    }
-
-    protected function tragedyExtraAmountByAudience(int $audience): Amount
-    {
-        if ($audience > 30) {
-            return new Amount(amount: 1000 * ($audience - 30));
-        }
-        return new Amount(0);
-    }
-
 }
