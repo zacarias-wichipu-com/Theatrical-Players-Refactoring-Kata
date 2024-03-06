e64f0277970c4af7f7375eb7b619db0af6f83952
Zacarías <zacarias@wichipu.com>
2024-03-06 07:04:59 +0100

    refactor: Improve Comedy code.

--- src/Comedy.php
+++ src/Comedy.php
@@ -13,24 +13,19 @@ final readonly class Comedy extends Play
 
     public function amount(int $audience): Amount
     {
-        return $this->comedyAmount($audience);
-    }
-
-    private function comedyAmount(int $audience): Amount
-    {
-        $feeAmount = $this->comedyFeeAmount($audience);
+        $feeAmount = $this->feeAmount($audience);
         return $feeAmount->add(
-            amountToAdd: $this->comedyExtraAmountByAudience($audience)
+            amountToAdd: $this->extraAmountByAudience($audience)
         );
     }
 
-    private function comedyFeeAmount(int $audience): Amount
+    private function feeAmount(int $audience): Amount
     {
         $amount = new Amount(amount: 30000);
         return $amount->add(new Amount(amount: 300 * $audience));
     }
 
-    public function comedyExtraAmountByAudience(int $audience): Amount
+    private function extraAmountByAudience(int $audience): Amount
     {
         if ($audience > 20) {
             return new Amount(amount: 10000 + 500 * ($audience - 20));
